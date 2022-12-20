a76b9b7d297e411f4e25ddfd2c6f5e8a0a4ba9b3
ntdung7899 <ntdung899@gmail.com>
2022-12-20 15:26:56 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import TextScreen from './src/TestScreen';
+const App = () => {
+  return (
+    <SafeAreaView>
+      <TextScreen />
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ src/TestScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const TestScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text> aaa</Text>
+    </SafeAreaView>
+  );
+};
+
+export default TestScreen;
